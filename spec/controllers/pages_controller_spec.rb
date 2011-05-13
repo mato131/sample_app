8e3fa06688324b08aa5298150484333b9928e0d8
Mohamed Loutfi <mato131@gmail.com>
2011-05-13 23:57:46 +0100

    Added a site layout

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,12 +1,22 @@
 require 'spec_helper'
 
 describe PagesController do
+render_views
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
+    it  "should have the right title" do
+      get 'home'
+      response.should have_selector ("title",
+                                     :content => "The Latest Debate | Home")
+      end
+  it "should have a non-blank body" do
+    get 'home'
+    response.body.should_not =~/<body>\s*<\/body>/
+  
   end
 
   describe "GET 'contact'" do
@@ -14,6 +24,24 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+    it  "should have the right title" do
+      get 'contact'
+      response.should have_selector ("title",
+                                     :content => "The Latest Debate | Contact")
+      end
   end
+  
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+    it  "should have the right title" do
+      get 'about'
+      response.should have_selector ("title",
+                                     :content => "The Latest Debate | About")
+      end
+    end
+  end
+  
 
-end
