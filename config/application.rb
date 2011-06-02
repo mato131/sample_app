808ee5172e64d487c19efb8d94ab0f7748df21a6
Mohamed Loutfi <mato131@gmail.com>
2011-06-02 23:40:56 +0100

    Initial interface for following/followers2

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,6 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module SampleApp
   class Application < Rails::Application
-    include Rake::DSL
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
